8dfcb9b1421166699537cff54a32040644f76e0a
bcell <brent.selby@gmail.com>
2017-02-08 20:27:39 -0500

    Change to d3 v3
    Also keeps script for using v4

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,7 @@
 var canvas = d3.select("canvas").node(),
     context = canvas.getContext("2d"),
-    path = d3.geoPath().projection(null).context(context);
+    // path = d3.geoPath().projection(null).context(context); //v4
+    path = d3.geo.path().projection(null).context(context); //v3
 
 d3.json("/javascripts/freer1.json", function(error, ca) {
   if (error) throw error;
