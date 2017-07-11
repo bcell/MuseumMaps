4d5210216febdca139d92fc1b9fe34f5c9c19611
Jason Fehr <fehrjason@gmail.com>
2017-07-11 09:54:33 -0400

    Modify d3, add hover and onclick, add gitignore
    Restructure d3 per the standards (g innercanvas, margins object, etc.)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,11 @@
+const infoModal = () => {
+  console.log('test function');
+  const body = d3.select('body')
+      .append('div')
+        .attr('class', 'infoModal');
+};
+
+
 d3.csv("data/freer.csv", function (error, roomData) {
   roomData.forEach(function (d) {
     d['Bottom'] = +d['Bottom'];
@@ -9,32 +17,50 @@ d3.csv("data/freer.csv", function (error, roomData) {
   });
   console.log(roomData);
 
-  var svg = d3.select("body").append("svg")
-            .attr("width", 1500)
-            .attr("height", 1300);
+  const museumRoomSpecs = {
+    width: 1500,
+    height: 1300
+  }
 
-  var rects = svg.selectAll("rect")
+  const svg = d3.select("body").append("svg")
+            .attr("width", museumRoomSpecs.width)
+            .attr("height", museumRoomSpecs.height);
+
+  const margin = { top: -150, right: 20, bottom: 20, left: 10 };
+  const width = svg.attr('width') - margin.left - margin.right;
+  const height = svg.attr('height') - margin.top - margin.bottom;
+
+  const g = svg.append('g')
+              .attr('transform', `translate(${margin.left},${margin.top})`)
+              .attr('class', 'innerCanvas');
+
+  var rects = g.selectAll("rect")
             .data(roomData)
             .enter()
             .append("rect");
 
   var rectAttributes = rects
             .attr("x", function (d) { return d.Left; })
-            .attr("y", function (d) { return (d.Top-200); })
+            .attr("y", function (d) { return (d.Top); })
             .attr("width", function (d) { return d.Width; })
             .attr("height", function (d) { return d.Height; })
-            .style("fill", "green");
+            .attr('class', 'museum-room')
+            .on('click', this.openModal);
 
-  var text = svg.selectAll("text")
+  var text = g.selectAll("text")
             .data(roomData)
             .enter()
             .append("text");
 
   var textLabels = text
-            .attr("x", function(d) { return d.Left; })
-            .attr("y", function(d) { return (d.Top-200); })
+            .attr("x", function(d) { return d.Left + (d.Width * 0.5); })
+            .attr("y", function(d) { return d.Top + (d.Height * 0.5); })
             .text(function (d) { return "Room #" + d.RoomNum; })
             .attr("font-family", "sans-serif")
-            .attr("font-size", "20px")
-            .attr("fill", "red");
+            .attr('text-anchor', 'middle')
+            .attr('class', 'museum-room-text');
 });
+
+function openModal(e) {
+  console.log('event: ', e);
+};
---
+++ public/javascripts/infoModal.js
@@ -0,0 +1,5 @@
+const infoModal = () => {
+  console.log('test function');
+};
+
+export default infoModal;
